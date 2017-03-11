c7fca5065c7a58927eeae9ba654247c62eac1d18
GaoRong <1543153960@qq.com>
2017-03-11 17:41:11 +0800

    NewCodeTest1

--- exam/README.md
+++ exam/README.md
@@ -29,11 +29,19 @@ address: https://www.nowcoder.com/questionTerminal/9d5fbe7750a34d0b91c73943f93b2
 
 
 #网易2016实习研发工程师   
-###二叉树   
+###[二叉树](https://github.com/GaoRong/AlgorithmTraining/blob/master/exam/BTleafNodeDistance.cpp)
 tag: 二叉树   
 address: https://www.nowcoder.com/questionTerminal/d567727f21a247f7b64ba32431cb9a19   
 >有一棵二叉树，树上每个点标有权值，权值各不相同，请设计一个算法算出权值最大的叶节点到权值最小的叶节点的距离。二叉树每条边的距离为1，一个节点经过多少条边到达另一个节点为这两个节点之间的距离。 给定二叉树的根节点root，请返回所求距离。    
-idea: 典型的二叉树递归也可解，将二叉树叶子结点进行01编码是妙解   
+tips: 典型的二叉树递归也可解，但将二叉树叶子结点进行01编码是妙解     
 
+###[比较重量](https://github.com/GaoRong/AlgorithmTraining/blob/master/exam/WeightCompare.cpp)
+tag: 有向图，多叉树，bfs，floyd算法  
+address: https://www.nowcoder.com/questionTerminal/ac27e60e63b549d6a9b59f815d9bc6e2       
+>给定两颗钻石的编号g1,g2，编号从1开始，同时给定关系数组vector,其中元素为一些二元组，第一个元素为一次比较中较重的钻石的编号，第二个元素为较轻的钻石的编号。最后给定之前的比较次数n。请返回这两颗钻石的关系，若g1更重返回1，g2更重返回-1，无法判断返回0。输入数据保证合法，不会有矛盾情况出现。    
+tips:  对于判断相互关系的题型都可以这样将一个构造一个有向图或多叉树      
+
+
+ 
 
 
---
+++ exam/WeightCompare.cpp
@@ -0,0 +1,105 @@
+/********************************************************************************
+** auth：  gaorong
+** mail：  gaoronggood@163.com
+** date：  2017-03-11 16:40:07
+** desc：  https://www.nowcoder.com/questionTerminal/ac27e60e63b549d6a9b59f815d9bc6e2
+*********************************************************************************/
+
+//思路一: 首先构造有向图，其中方向标识大到小，然后利用Floyd算法构造完整的层次关系
+class Cmp {
+public:
+    int cmp(int g1, int g2, vector<vector<int> > records, int n) {
+		
+        int maxNum = -10000;
+        //首先获取最大值
+        for(int i=0; i<n; i++){
+            maxNum = max( records[i][0], maxNum);
+            maxNum = max( records[i][1], maxNum );
+        }
+        
+        //构造有向图邻接矩阵
+        int matrics[maxNum+1][maxNum+1];	//如果编译不过可以使用vector
+        memset(matrics,0,sizeof(matrics));
+        
+        for(int i=1; i<=maxNum; i++){
+            matrics[i][i] = 1;
+        }
+        
+        for(int i=0; i<n; i++){
+        	matrics[ records[i][0] ][ records[i][1] ] = 1;
+        }
+        
+        //利用Floyd算法进行关系的递推
+        for(int i=1; i<=maxNum; i++){
+            for(int j=1; j<=maxNum; j++){
+                for(int k=1; k<=maxNum; k++){
+                    if( matrics[i][k]==1 && matrics[k][j]==1 ){
+                        matrics[i][j] = 1;
+                    }
+                }
+                
+            }
+        }
+        
+        if( matrics[g1][g2]==1 ){
+            return 1;
+        }else if( matrics[g2][g1]==1 ){
+            return -1;
+        }else{
+            return 0;
+        }
+        
+    
+    }
+};
+
+#include <unordered_map>
+class Cmp2 {
+public:
+    bool check(unordered_map<int,vector<int>>& m, int g1, int g2 ){
+    	
+        unordered_map<int,bool> mark;
+        queue<int> q;
+        q.push( g1 );
+        mark[g1] = true;
+        
+        while( !q.empty() ){
+            int val = q.front();
+            q.pop();
+          	mark[val] = true;
+            if( val == g2 )
+                return true;
+           
+            for(int i=0; i<m[val].size(); i++){
+            	if( mark[ m[val][i] ] == false ){
+                    mark[ m[val][i] ] = true;
+                    q.push( m[val][i] );
+                }
+            }            
+        }
+        
+        return false;
+        
+    } 
+        
+    int cmp(int g1, int g2, vector<vector<int> > records, int n) {
+		
+        //利用多叉树保存大小关系，叶节点的值比根的重量轻
+        unordered_map<int,vector<int>> m;
+        
+        for(int i=0; i<n; i++){
+            m[ records[i][0] ].push_back( records[i][1] );
+        }
+        
+		//进行BFS层次遍历，判断是否为子树
+        if( check( m, g1,g2 ) ){
+            return 1;
+        }
+        else if( check( m, g2, g1 ) ){
+            return -1;
+        }else{
+            return 0;
+        }
+    
+    }
+};
