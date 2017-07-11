d994390d4d4b9eeed224cc2d212952da0c0a4cc5
GaoRong <1543153960@qq.com>
2017-07-11 22:27:42 +0800

    modify tool code readme

--- tools/ComplexStringAlg.cpp
+++ tools/ComplexStringAlg.cpp
@@ -5,15 +5,13 @@
 ** desc：  字符串高级算法
 *********************************************************************************/
 
-
 //最长回文字串（Longest Palindromic Substring）
 //从中心扩展的方式，时间复杂度O(n^2),空间复杂度O(1)
 //详细参见：https://leetcode.com/articles/longest-palindromic-substring/ 
 string longestPalindrome(string s) {
         int res = -1;
         int l=0;
-        
-        
+         
         for(int i = 0; i<s.length(); i++){
             int k = i-1,j=i+1;
             
@@ -21,20 +19,17 @@ string longestPalindrome(string s) {
                 k--;
                 j++;
             }
-            
             if( res < j-k-1)
             {
                 l = k+1;
                 res = j-k-1;
             }
             
-            
             k = i,j=i+1;
             while( k>=0 && j<s.length() && s[k] == s[j] ){
                 k--;
                 j++;
             }
-            
             if( res < j-k-1)
             {
                 l = k+1;
@@ -43,5 +38,5 @@ string longestPalindrome(string s) {
                        
         }
           return s.substr(l,res);
-    }
-    
+   }
+   
--- tools/README.md
+++ tools/README.md
@@ -1,5 +1,14 @@
 # Tool Code
 This folder contain some tool code which will help
 us achieve the goal of agile software development.
+       
+#### 字符串操作  string.h         
+> 以指点字符分割字符串，返回vector数组     
+
+#### 字符串高级操作 ComplexStringAlg.cpp    
+>  最长回文字串    
+
+
+
 
 
